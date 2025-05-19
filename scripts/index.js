afe30b9471672a660af57b98bf405b5e6caafeab
Mona Abdelreheem <m.abdelraheem@aol.com>
2025-05-19 01:58:12 -0500

    Remove mood selection section and  related styles

--- scripts/index.js
+++ scripts/index.js
@@ -31,26 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
   cancelHabitBtn.addEventListener("click", hideHabitModal);
 });
 
-// experimenting the feature
-
-function handleMoodSelection(event) {
-  const mood = event.currentTarget.dataset.mood;
-
-  document.body.setAttribute("data-mood", mood);
-  moodPopup.classList.add("mood--hidden");
-  dashboard.classList.remove("dashboard--hidden");
-}
-
-let moodBtns, moodPopup, dashboard;
-
-document.addEventListener("DOMContentLoaded", () => {
-  moodBtns = document.querySelectorAll(".mood__btn");
-  moodPopup = document.getElementById("mood");
-  dashboard = document.getElementById("dashboard");
-  moodBtns.forEach((btn) => {
-    btn.addEventListener("click", handleMoodSelection);
-  });
-});
 let lastSpinDegrees = 0;
 const spinWheel = document.querySelector(".spinwheel__box");
 const videoModal = document.querySelector(".modal");
